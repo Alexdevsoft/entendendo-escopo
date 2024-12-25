3f1d8c9e3b13c19d58c6cab91579d31ecea23bad
Alexdevsoft <alexhavilla2022@gmail.com>
2024-12-25 19:05:50 -0300

    Variável alterada

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// A variável não pode ficar fora do escopo ou deve ser inicializada.
 		double preco = 30;
 		double desconto;
 		
 		if(preco > 20) {
-			desconto = preco * 0.1;
+			desconto = preco * 0.2;
 		}
 		else {
 			desconto = 0;
